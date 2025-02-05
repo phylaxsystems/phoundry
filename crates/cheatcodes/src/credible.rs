cb68cf41c2632b0261876e34656278a97cfc0186
Odysseas.eth <odyslam@gmail.com>
2025-02-05 17:44:11 +0200

    chore: cleanup

--- crates/cheatcodes/src/credible.rs
+++ crates/cheatcodes/src/credible.rs
@@ -1,13 +1,19 @@
 use crate::{Cheatcode, CheatcodesExecutor, CheatsCtxt, Result, Vm::*};
-use alloy_primitives::{hex::hex, TxKind};
+use alloy_primitives::TxKind;
 use alloy_sol_types::{Revert, SolError, SolValue};
 use assertion_executor::{db::fork_db::ForkDb, store::MockStore, ExecutorConfig};
-use foundry_evm_core::{backend::{DatabaseError, DatabaseExt, GLOBAL_FAIL_SLOT}, constants::CHEATCODE_ADDRESS};
+use foundry_evm_core::{
+    backend::{DatabaseError, DatabaseExt, GLOBAL_FAIL_SLOT},
+    constants::CHEATCODE_ADDRESS,
+};
 use revm::{
     primitives::{AccountInfo, Address, Bytecode, ExecutionResult, TxEnv, B256, U256},
     DatabaseCommit, DatabaseRef,
 };
-use std::{collections::HashMap, sync::{Arc, Mutex}};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use tokio;
 
 /// Wrapper around DatabaseExt to make it thread-safe
@@ -53,7 +59,12 @@ impl<'a> DatabaseRef for ThreadSafeDb<'a> {
 
 impl Cheatcode for assertionExCall {
     fn apply_full(&self, ccx: &mut CheatsCtxt, executor: &mut dyn CheatcodesExecutor) -> Result {
-        let Self { tx, assertionAdopter: assertion_adopter, assertionContract, assertionContractLabel } = self;
+        let Self {
+            tx,
+            assertionAdopter: assertion_adopter,
+            assertionContract,
+            assertionContractLabel,
+        } = self;
 
         let spec_id = ccx.ecx.spec_id();
         let block = ccx.ecx.env.block.clone();
@@ -69,7 +80,9 @@ impl Cheatcode for assertionExCall {
         let config = ExecutorConfig { spec_id, chain_id, assertion_gas_limit: 3_000_000 };
 
         let mut store = MockStore::new(config.clone());
-        store.insert(*assertion_adopter, vec![assertion_contract_bytecode]).expect("Failed to store assertions");
+        store
+            .insert(*assertion_adopter, vec![assertion_contract_bytecode])
+            .expect("Failed to store assertions");
 
         let decoded_tx = AssertionExTransaction::abi_decode(&tx, true)?;
 
@@ -108,44 +121,72 @@ impl Cheatcode for assertionExCall {
 
             validate_result
         });
-        if res.is_err(){
+        if res.is_err() {
             bail!("Error during Assertion Execution: {:#?}", res.err().unwrap());
         }
         let tx_validation = res.unwrap();
+        let assertion_contract = tx_validation.assertions_executions.first().unwrap();
         let total_assertion_gas = tx_validation.total_assertions_gas();
         let total_assertions_ran = tx_validation.total_assertion_funcs_ran();
-        if !tx_validation.is_valid(){
+        let mut assertion_gas_message = format!(
+            "Assertion Functions gas cost\n  Total gas cost: {}\n  Total assertions ran: {}\n",
+            total_assertion_gas, total_assertions_ran
+        );
+
+        // Format individual assertion function results
+        for (fn_selector_index, assertion_fn) in
+            assertion_contract.assertion_fns_results.iter().enumerate()
+        {
+            assertion_gas_message.push_str(&format!(
+                "   └─ [selector {}:index {}] gas cost: {}\n",
+                assertion_fn.id.fn_selector,
+                fn_selector_index,
+                assertion_fn.as_result().gas_used()
+            ));
+        }
+        executor.console_log(ccx, assertion_gas_message);
+
+        if !tx_validation.is_valid() {
             if !tx_validation.result_and_state.result.is_success() {
                 let decoded_error = decode_revert_error(&tx_validation.result_and_state.result);
                 bail!("Transaction Execution Reverted: {}", decoded_error.reason());
             }
-            let mut reverted_assertions = HashMap::new();
-            // There should only be one assertion contract in the tx validation
-            let assertion_contract = tx_validation.assertions_executions.first().unwrap();
-            for (fn_selector_index, assertion_fn) in assertion_contract.assertion_fns_results.iter().enumerate() {
-                if !assertion_fn.is_success() {
-                    let key = format!("[selector {}:index {}]", assertion_fn.id.fn_selector, fn_selector_index);
+
+            let mut error_msg = format!("\n  {assertionContractLabel} Assertions Failed:\n");
+
+            // Collect failed assertions
+            let reverted_assertions: HashMap<_, _> = assertion_contract
+                .assertion_fns_results
+                .iter()
+                .enumerate()
+                .filter(|(_, assertion_fn)| !assertion_fn.is_success())
+                .map(|(fn_selector_index, assertion_fn)| {
+                    let key = format!(
+                        "[selector {}:index {}]",
+                        assertion_fn.id.fn_selector, fn_selector_index
+                    );
                     let revert = decode_revert_error(assertion_fn.as_result());
-                    reverted_assertions.insert(key, revert);
-                }
-            }
-            let mut error_msg = String::from("\n{assertionContractLabel} Assertions Failed:\n");
+                    (key, revert)
+                })
+                .collect();
+
+            // Format error messages
             for (key, revert) in reverted_assertions {
                 error_msg.push_str(&format!(
-                    "{} - Revert Reason: {} \n", 
-                    key, revert.reason()
+                    "   └─ {} - Revert Reason: {} \n",
+                    key,
+                    revert.reason()
                 ));
             }
+
             executor.console_log(ccx, error_msg);
             ccx.ecx.sstore(CHEATCODE_ADDRESS, GLOBAL_FAIL_SLOT, U256::from(1))?;
         }
-        Ok((total_assertion_gas, total_assertions_ran).abi_encode())
+        Ok(Default::default())
     }
 }
 
 fn decode_revert_error(revert: &ExecutionResult) -> Revert {
     Revert::abi_decode(&revert.clone().into_output().unwrap_or_default(), false)
-        .unwrap_or(Revert::new((
-            "Unknown Revert Reason".to_string(),
-        )))
+        .unwrap_or(Revert::new(("Unknown Revert Reason".to_string(),)))
 }
